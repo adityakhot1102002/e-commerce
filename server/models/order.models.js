1bde00e681ee9dcd5cc6b333676a5e983ecad518
Aditya Khot <aditya.khot@bighaat.com>
2024-08-27 11:05:53 +0530

    Added server files with only models

---
+++ server/models/order.models.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+const orderItemSchema = new mongoose.Schema({
+    productid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+const orderSchema = new mongoose.Schema({
+    orderid: {
+        type: Number,
+        required: true
+    },
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    orderItems: {
+        type: [orderItemSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+export const Order = mongoose.Order("Order", orderSchema)
